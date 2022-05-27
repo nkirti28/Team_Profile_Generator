03e3af61d3e232aad131397582a7ceb0ea973309
nkirti28 <nalawade.kirti@gmail.com>
2022-05-27 18:38:50 -0500

    test classes added

---
+++ test/intern.test.js
@@ -0,0 +1,42 @@
+// using Intern constructor
+const Intern = require("../lib/Intern");
+
+describe("Intern", () => {
+  // creates Intern object
+  test("creates an Intern object", () => {
+    const intern = new Intern(
+      "Kirti",
+      34,
+      "nalawade.kirti@gmail.com",
+      "nkirti28"
+    );
+
+    expect(intern.empGitHub).toEqual(expect.any(String));
+  });
+
+  // gets github_username from getGithub()
+  test("gets github username of Intern", () => {
+    const intern = new Intern(
+      "Kirti",
+      34,
+      "nalawade.kirti@gmail.com",
+      "nkirti28"
+    );
+
+    expect(intern.empGitHub).toEqual(
+      expect.stringContaining(Intern.empGitHub.toString())
+    );
+  });
+
+  // gets role from getRole()
+  test("gets role of Intern", () => {
+    const intern = new Intern(
+      "Kirti",
+      34,
+      "nalawade.kirti@gmail.com",
+      "nirti28"
+    );
+
+    expect(intern.getRole()).toEqual("Intern");
+  });
+});
